02f13eae2326ff821e63cb00331cd0639a58bdd4
rbrossi <rbrossi@br.ibm.com>
2018-10-22 15:54:05 +0200

    Test

--- provision.sh
+++ provision.sh
@@ -9,8 +9,8 @@ sudo apt-get install docker-engine --force-yes -y
 sudo usermod -aG docker vagrant
 sudo service docker start
 docker version
-curl -L http://portainer.io/download/portainer-agent-stack.yml -o portainer-agent-stack.yaml
-docker stack deploy --compose-agent-stack.yml portainer
+# curl -L http://portainer.io/download/portainer-agent-stack.yml -o portainer-agent-stack.yaml
+# docker stack deploy --compose-file=portainer-agent-stack.yaml portainer
 
 
 
