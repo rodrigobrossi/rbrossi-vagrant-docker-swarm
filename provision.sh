c181c6e12b036aadd43fc2022a852bc386e8b9b6
rbrossi <rbrossi@br.ibm.com>
2018-10-25 10:38:22 +0200

    Delete unessary files

--- provision.sh
+++ provision.sh
@@ -9,8 +9,9 @@ sudo apt-get install docker-engine --force-yes -y
 sudo usermod -aG docker vagrant
 sudo service docker start
 docker version
-curl -L http://portainer.io/download/portainer-agent-stack.yml -o portainer-agent-stack.yaml
-docker stack deploy --compose-agent-stack.yml portainer
+docker pull cockroachdb/cockroach:v2.0.6
+# curl -L http://portainer.io/download/portainer-agent-stack.yml -o portainer-agent-stack.yaml
+# docker stack deploy --compose-file=portainer-agent-stack.yaml portainer
 
 
 
